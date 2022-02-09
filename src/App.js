63f69f765163547c023967698f4b0caad249d1f1
Badree007 <badreedahal1@gmail.com>
2022-02-10 10:46:30 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { AiOutlineSetting } from 'react-icons/ai';
+import { RiStethoscopeLine } from 'react-icons/ri';
+import { FaWrench } from 'react-icons/fa';
+import { GrCatalogOption } from 'react-icons/gr';
+import { VscGraphLine } from 'react-icons/vsc';
+
+function Header() {
+  return (
+    <header className="main-header">
+        <h1>Welcome</h1>
+    </header>
+  )
+}
+
+function SideBar() {
+  return (
+    <aside className="sidebar">
+      <div className="side-content">
+        <ul>
+          <li><a href={"#"}>
+           <VscGraphLine size={`2em`}/> 
+           <p>Reports</p>
+          </a></li>
+          <li><a href={"#"}>
+           <FaWrench size={`2em`} style={{fill: 'white', strokeWidth: '2rem'}}/> 
+           <p>Strategy</p>
+          </a></li>
+          <li><a href={"#"}>
+           <RiStethoscopeLine size={`2em`}/> 
+           <p>Assessments</p>
+          </a></li>
+          <li><a href={"#"}>
+           <GrCatalogOption size={`2em`}/> 
+           <p>Catalogue</p>
+          </a></li>
+          <li><a href={"#"}>
+           <AiOutlineSetting size={`2em`}/> 
+           <p>Settings</p>
+          </a></li>
+        </ul>
+      </div>
+    </aside>
+  )
+}
+
+function ObjectiveBox() {
+  return (
+    <div className='objective-box'>
+      
+      <div className='objective'>
+        <h4>Objective 1</h4>
+        <input type='text' name='objective'/>
+      </div>
+
+      <div className='date-field'>
+        <div className='start-date-field'>
+          <h4>Start Date</h4>
+          <input type='date' name='start-date'/>
+        </div>
+        <div className='end-date-field'>
+          <h4>End Date</h4>
+          <input type='date' name='end-date'/>
+        </div>
+      </div>
+
+      <div className='keyMeasure-field'>
+        <h4>Key Measures</h4>
+        <input type='text' name='key-measure'/>
+      </div>
+      <div className='update-btn-field'>
+        <button className='update-btn'>Update</button>
+      </div>
+
+    </div>
+  )
+}
+
+function MainBody() {
+  return (
+    <div className='main-body'>
+      <p className='main-top'>Set Security Strategy</p>
+      <div className='content-wrapper'>
+        <ObjectiveBox/>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <main className='main-content'>
+        <SideBar/>
+        <MainBody/>
+      </main>
     </div>
   );
 }
